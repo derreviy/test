f200a8436bd3153ef416b74319a9cb955e26ab15
Viktor Varenik <yavarenikya@gmail.com>
2024-10-19 21:09:11 +0300

    chore(refactor): split some main logic to separated files and replace token

---
+++ src/main/kotlin/ContactsRepository.kt
@@ -0,0 +1,72 @@
+package com.github.kotlintelegrambot
+
+import com.google.gson.Gson
+import java.io.File
+
+class ContactsRepository {
+    private val gson = Gson()
+    private val file = File("contacts.json")
+    private val contacts = arrayListOf<Contact>()
+
+    fun load() {
+        if(!file.exists()) file.createNewFile()
+
+
+        val text2 = file.readText()
+        if(text2.isNotEmpty()) {
+            val contacts2 = gson.fromJson(text2, ContactsArray::class.java)
+            val contactsArray2: ArrayList<Contact> = contacts2.contacts
+
+            contacts.clear()
+            contacts.addAll(contactsArray2)
+            println(contacts)
+        }
+    }
+
+    fun save() {
+        val contactsArray = ContactsArray(contacts)
+        val text = gson.toJson(contactsArray)
+        file.writeText(text)
+    }
+
+    fun getAll(): ArrayList<Contact> {
+        return contacts
+    }
+
+    fun add(name: String,phone: String) {
+        contacts.add(Contact(name, phone))
+    }
+
+    fun search(query: String): String {
+        var rezultat = ""
+        val serthRez = contacts.find { it.name == query }
+
+
+        if (serthRez != null) {
+            val rezult = "\n${serthRez.name} " + ":" + " ${serthRez.phone}"
+            rezultat = rezult
+        }
+        else {
+            val  notFound = "\nnot found"
+            return  notFound
+
+        }
+        return rezultat
+
+    }
+    fun seeContact(info: String): String {
+        var allCon = ""
+        for ((id, con) in contacts.withIndex()) {
+
+            val see = "\n" + id.toString() + "  " + con.name + ":" + con.phone
+            allCon += see
+        }
+        return allCon
+    }
+    fun deleteContact(query: String): String {
+        val dc = contacts.get(query.toInt()).name
+        contacts.removeAt(query.toInt())
+        save()
+        return dc
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,88 +7,10 @@ import com.github.kotlintelegrambot.extensions.filters.Filter
 import com.google.gson.Gson
 import java.io.File
 
-const val BOT_TOKEN = "7376283756:AAH6bsYn2N_DgunpNx3BLA3EFVUULiB1T6U"
-
-sealed interface State {
-    data object DefaultState: State
-    data object WaitingNameState: State
-    data class WaitingPhoneState(val name: String): State
-    data object WaitingSearthNameState: State
-    data object WaitingDeleteNameState: State
-
-}
+const val BOT_TOKEN = "7212513557:AAFFNPYpgZNn28R2-uefAJaKUnydKq6T5Ag"
 
 data class Contact(val name: String, val phone: String)
 data class ContactsArray(val contacts: ArrayList<Contact>)
-
-class ContactsRepository {
-    private val gson = Gson()
-    private val file = File("contacts.json")
-    private val contacts = arrayListOf<Contact>()
-
-    fun load() {
-        if(!file.exists()) file.createNewFile()
-
-
-        val text2 = file.readText()
-        if(text2.isNotEmpty()) {
-            val contacts2 = gson.fromJson(text2, ContactsArray::class.java)
-            val contactsArray2: ArrayList<Contact> = contacts2.contacts
-
-            contacts.clear()
-            contacts.addAll(contactsArray2)
-            println(contacts)
-        }
-    }
-
-    fun save() {
-        val contactsArray = ContactsArray(contacts)
-        val text = gson.toJson(contactsArray)
-        file.writeText(text)
-    }
-
-    fun getAll(): ArrayList<Contact> {
-        return contacts
-    }
-
-    fun add(name: String,phone: String) {
-        contacts.add(Contact(name, phone))
-    }
-
-    fun search(query: String): String {
-        var rezultat = ""
-        val serthRez = contacts.find { it.name == query }
-
-
-        if (serthRez != null) {
-            val rezult = "\n${serthRez.name} " + ":" + " ${serthRez.phone}"
-            rezultat = rezult
-        }
-        else {
-            val  notFound = "\nnot found"
-            return  notFound
-
-        }
-        return rezultat
-
-    }
-    fun seeContact(info: String): String {
-        var allCon = ""
-        for ((id, con) in contacts.withIndex()) {
-
-            val see = "\n" + id.toString() + "  " + con.name + ":" + con.phone
-            allCon += see
-        }
-        return allCon
-    }
-    fun deleteContact(query: String): String {
-        val dc = contacts.get(query.toInt()).name
-        contacts.removeAt(query.toInt())
-        save()
-        return dc
-    }
-}
-
 
 fun main() {
     println("Started")
